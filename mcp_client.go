24c0b689be8b46a3d971b63da6e78e132d5813fd
kushagr <skushagr15@gmail.com>
2026-01-11 00:46:43 +0530

    add MCP client abstraction for filesystem tools

---
+++ mcp_client.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"os/exec"
+)
+
+type MCPClient struct {
+	cmd    *exec.Cmd
+	stdin  any
+	stdout any
+	nextID int
+}
+
+func NewMCPClient(allowedpath string) (*MCPClient, error) {
+	cmd := exec.Command("mcp-filesystem-server", allowedpath)
+
+	stdin, _ := cmd.StdinPipe()
+	stdout, _ := cmd.StdoutPipe()
+
+	err := cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+	return &MCPClient{
+		cmd:    cmd,
+		stdin:  stdin,
+		stdout: stdout,
+		nextID: 1,
+	}, nil
+}
+
+//NOTE:ToolCall is a function or method of struct MCPClient
+
+func (c *MCPClient) ToolCall(name string, params map[string]any) ([]byte, error) {
+	request := map[string]any{
+		"jsonrpc": "2.0",
+		"id":      c.nextID,
+		"method":  "tools/call",
+		"params": map[string]any{ //function name and arguments are passed
+			"name":      name,
+			"arguments": params,
+		},
+	}
+	c.nextID++
+
+	// Fix: Add type assertion for io.Writer
+	json.NewEncoder(c.stdin.(io.Writer)).Encode(request)
+	// Fix: Add type assertion for io.WriteCloser
+
+	c.stdin.(io.WriteCloser).Close() //tells the mcp no more request
+
+	//  READ THE FULL RESPONSE FROM THE MCP SERVER 
+	buf := make([]byte, 1024) //data comes in chunks
+	out := []byte{}
+
+	for {
+		n, err := c.stdout.(io.Reader).Read(buf)
+		if n > 0 {
+			out = append(out, buf[:n]...)
+		}
+		if err != nil {
+			break
+		}
+	}
+	return out, nil
+
+}
+
+
